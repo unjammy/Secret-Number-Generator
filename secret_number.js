3549989d5cebc696a1dcf2e2c7f97ed973e0673b
clarkdenunzio <clark@clarkdenunzio.com>
2017-05-18 19:45:08 -1000

    fdsfasg

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,22 @@
 'use strict';
 module.exports = function() {
-  
+
+  var secretNumberGenerator = (function(){
+    var secretNumber = 0;
+    return function(){
+      if(secretNumber === 0){
+        secretNumber = Math.random() % 1000000;
+      }
+      return secretNumber;
+    };
+  })();
+
+  function secretNumber(){
+    var secretNumber = 0;
+    if(secretNumber === 0){
+      secretNumber = Math.random() % 1000000;
+    }
+    return secretNumber;
+  }
+
 };
